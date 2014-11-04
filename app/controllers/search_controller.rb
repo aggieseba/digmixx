bb9673563ec49003eb701aad405ac54ac68b4639
shoyasono <noren.member@gmail.com>
2014-11-04 15:32:50 -0800

    cssファイルを修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,6 +5,7 @@ class SearchController < ApplicationController
   def show
     if params[:tag]
       @mixes = Mix.tagged_with(params[:tag])
+      @result = @mixes.name
     else
       @mixes = Mix.all
     end
