398fbe8bfcb1eafecd9c402b70a9edc2d0c443c8
aggieseba <aggieseba@gmail.com>
2014-11-13 20:57:05 -0800

    routesを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/show' => 'home#show'
-  get 'search/index'
   get 'search/show'
-  resources :home
-
   resources :search
-  resources :mixes
   root 'home#index'
-
 end
